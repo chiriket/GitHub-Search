cf6572bae323a62af0ed509d19923b949b6ea99c
Shirley Keter <shirleyketer@gmail.com>
2019-03-30 14:06:19 +0300

    added http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http'
 
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
-import { SearchService } from './searches/search.service';
+import { SearchService } from './searches/search.service'
+
 
 @NgModule({
   declarations: [
@@ -14,6 +16,7 @@ import { SearchService } from './searches/search.service';
   imports: [
     BrowserModule,
     FormsModule
+    
 
   ],
   providers: [SearchService],
--- src/app/searches/search.service.ts
+++ src/app/searches/search.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { Search } from '../search'
-
+import { Http, Headers } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class SearchService {

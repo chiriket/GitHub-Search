a6a9347c688ce15acedd4bf0e4db12419f3f6a17
Shirley Keter <shirleyketer@gmail.com>
2019-03-30 13:30:52 +0300

    added service to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { SearchComponent } from './search/search.component';
+import { SearchService } from './searches/search.service';
 
 @NgModule({
   declarations: [
--- src/app/searches/search.service.ts
+++ src/app/searches/search.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import { Search } from '../search'
 
-@Injectable({
-  providedIn: 'root'
-})
+
+@Injectable()
 export class SearchService {
 
-  constructor() { }
+  private username: string;
+  private clientid = '4193ec64f95f88022fb3';
+  private clientsecret = '5ab8a176a8d93c1c740400f04c70d57bfee1eb82';
+  constructor(private http: Http) {
+    console.log('Service is ready!');
+    this.username = 'Imma7';
+  }
+
+  getProfileInfo() {
+    // tslint:disable-next-line:max-line-length
+    return this.http.get('https://api.github.com/users/' + this.username + '?client_id=' + this.clientid + '&client_secret=' + this.clientsecret)
+    .map(res => res.json());
+  }
+
+  getProfileRepos() {
+    // tslint:disable-next-line:max-line-length
+    return this.http.get('https://api.github.com/users/' + this.username + '/repos?client_id=' + this.clientid + '&client_secret=' + this.clientsecret)
+    .map(res => res.json());
+  }
+
+  updateProfile(username: string) {
+    this.username = username;
+  }
+
 }

c60c2f8704ed86f3219d82f3feb27028c91f13b9
Shirley Keter <shirleyketer@gmail.com>
2019-04-01 15:44:49 +0300

    added bootstrap css styles

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl:'https://api.github.com/users/daneden?access_token=' +  '63080effc016467bfebda70c2883a8bf1fa1e0d5'
 };
